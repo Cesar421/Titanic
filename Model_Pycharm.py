480d8175c27944dcf9b316bc720b862c81cf9f74
Cesar.G <cesarcesargamba@gmail.com>
2022-08-15 15:51:54 -0500

    V2

--- Model_Pycharm.py
+++ Model_Pycharm.py
@@ -1,13 +1,14 @@
 import numpy as np
 import pandas as pd
 
-print('NumPy', np.__version__)
-print('pandas', pd.__version__)
+#print('NumPy', np.__version__)
+#print('pandas', pd.__version__)
 
 df_test = pd.read_csv("/home/cesarfernando/Cesar/Kaggle/Titanic/test.csv")
-df_train = pd.read_csv("/home/cesarfernando/Cesar/Kaggle/Titanic/train.csv")
-print(df_test.head)
-print(df_train.head)
+df_train_oroginal = pd.read_csv("/home/cesarfernando/Cesar/Kaggle/Titanic/train.csv")
 
-df_test.info()
+df_train_oroginal.info()
+df_train = df_train_oroginal.copy()
+del df_train['Cabin']
+df_train=df_train.dropna(subset=['Age', 'Embarked'])
 df_train.info()
