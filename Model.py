744dd6b190c85a85967169fc1c37b79d2364bdcf
Cesar.G <cesarcesargamba@gmail.com>
2022-08-15 11:46:30 -0500

    Jupyter change IDE

--- Model.py
+++ Model.py
@@ -7,10 +7,14 @@ import pandas as pd
 import numpy as np
 import pandas as pd
 
+print('NumPy', np.__version__)
+print('pandas', pd.__version__)
+
 df_test = pd.read_csv("/home/cesarfernando/Cesar/Kaggle/Titanic/test.csv")
 df_train = pd.read_csv("/home/cesarfernando/Cesar/Kaggle/Titanic/train.csv")
 print(df_test.head)
 print(df_train.head)
 
-
+df_test.info()
+df_train.info()
 
