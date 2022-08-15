4dac108ec51f04c7df12477263d593702e0d8b19
Cesar.G <cesarcesargamba@gmail.com>
2022-08-15 18:10:52 -0500

    VScode

--- Model_Pycharm.py
+++ Model_Pycharm.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pandas as pd
 
-#print('NumPy', np.__version__)
-#print('pandas', pd.__version__)
 
 df_test = pd.read_csv("/home/cesarfernando/Cesar/Kaggle/Titanic/test.csv")
 df_train_oroginal = pd.read_csv("/home/cesarfernando/Cesar/Kaggle/Titanic/train.csv")
@@ -12,3 +10,17 @@ df_train = df_train_oroginal.copy()
 del df_train['Cabin']
 df_train=df_train.dropna(subset=['Age', 'Embarked'])
 df_train.info()
+col = ['Survived', 'Pclass', 'Sex', 'SibSp']
+for i in col:
+    print(df_train[i].unique())
+
+women = df_train.loc[df_train.Sex == 'female']["Survived"]
+male = df_train.loc[df_train.Sex == 'male']["Survived"]
+#print(women.shape)
+#print(sum(women))
+#print(len(women))
+#print(women)
+rate_women = sum(women)/len(women)
+rate_male = sum(male)/len(male)
+print(rate_male*100, rate_women*100)
+
