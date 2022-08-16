5810d51af906ca5b1b5f8a2bb7d0515a4302526b
Cesar.G <cesarcesargamba@gmail.com>
2022-08-15 20:04:40 -0500

    VScode v2

--- Model_Pycharm.py
+++ Model_Pycharm.py
@@ -1,14 +1,14 @@
 import numpy as np
 import pandas as pd
-
+from sklearn.ensemble import RandomForestClassifier
 
 df_test = pd.read_csv("/home/cesarfernando/Cesar/Kaggle/Titanic/test.csv")
-df_train_oroginal = pd.read_csv("/home/cesarfernando/Cesar/Kaggle/Titanic/train.csv")
+df_train_original = pd.read_csv("/home/cesarfernando/Cesar/Kaggle/Titanic/train.csv")
 
-df_train_oroginal.info()
-df_train = df_train_oroginal.copy()
+df_train_original.info()
+df_train = df_train_original.copy()
 del df_train['Cabin']
-df_train=df_train.dropna(subset=['Age', 'Embarked'])
+df_train = df_train.dropna(subset=['Age', 'Embarked'])
 df_train.info()
 col = ['Survived', 'Pclass', 'Sex', 'SibSp']
 for i in col:
@@ -16,11 +16,21 @@ for i in col:
 
 women = df_train.loc[df_train.Sex == 'female']["Survived"]
 male = df_train.loc[df_train.Sex == 'male']["Survived"]
-#print(women.shape)
-#print(sum(women))
-#print(len(women))
-#print(women)
+
 rate_women = sum(women)/len(women)
 rate_male = sum(male)/len(male)
 print(rate_male*100, rate_women*100)
 
+y = df_train["Survived"]
+
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+X = pd.get_dummies(df_train[features])
+X_test = pd.get_dummies(df_train[features])
+
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model.fit(X, y)
+predictions = model.predict(X_test)
+
+output = pd.DataFrame({'PassengerId': df_test.PassengerId, 'Survived': predictions})
+output.to_csv('my_submission.csv', index=False)
+print("Your submission was successfully saved!")
